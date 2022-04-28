d7fc8311873837289383146b3a96ea1c0f6998bb
fedeprat <97050765+fedeprat@users.noreply.github.com>
2022-04-27 22:43:03 -0300

    added some more projects

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -5,6 +5,8 @@ import AnimatedLetters from '../animatedLetters'
 import NFTickets from '../assets/imagesProjects/NFTickets.png'
 import ReañtorAssistantBase from '../assets/imagesProjects/RealtorAssistant.png'
 import ReañtorAssistantConfigured from '../assets/imagesProjects/RealtorAssistantConfigured.png'
+import dogsDatabase from '../assets/imagesProjects/dogsdatabase.png'
+import moviesdatabase from '../assets/imagesProjects/moviesplace.png'
 
 export default function Projects() {
   const [letterClass, setLetterClass] = useState('text-animate')
@@ -18,7 +20,7 @@ export default function Projects() {
 
   return (
     <>
-      <div style={{ height: '100%', overflow: 'auto' }}>
+      <div style={{ height: '100%', overflow: 'auto' }} className='containerdetodo'>
         <h1>
           <AnimatedLetters
             letterClass={letterClass}
@@ -40,11 +42,10 @@ export default function Projects() {
               <br /> We use the Ropsten's test network as blockchain, to see how
               to switch from your current blockchain in metamask head to "quick
               start" section. <br />
-              We also have a video tour through the app to see all the features.{' '}
+              We also have a video tour through the app to see all the features.
               <br />
               App deployed:{' '}
-              <a href="https://nftickets.netlify.app/">NFTickets</a> | github
-              repo:{' '}
+              <a href="https://nftickets.netlify.app/">NFTickets</a> |{' '}
               <a href="https://github.com/fedeprat/NFTickets">gitHub repo</a>
             </p>
           </div>
@@ -56,11 +57,15 @@ export default function Projects() {
           <img
             className="realtor"
             src={ReañtorAssistantConfigured}
-            alt="NFTickets"
+            alt="ReañtorAssistantConfigured"
           />
-          <img className="realtor" src={ReañtorAssistantBase} alt="NFTickets" />
+          <img
+            className="realtor"
+            src={ReañtorAssistantBase}
+            alt="ReañtorAssistantBase"
+          />
           <div style={{ position: 'relative', width: '1500px' }}>
-          <h2>Realtor Assistant</h2>
+            <h2>Realtor Assistant</h2>
             <br />
             <p className="description">
               RealtorAssistant was born because my brother is a real estate
@@ -81,21 +86,56 @@ export default function Projects() {
         <hr />
 
         <div className="individualProjects">
-          <img
-            className="realtor"
-            src={ReañtorAssistantConfigured}
-            alt="NFTickets"
-          />
-          <img className="realtor" src={ReañtorAssistantBase} alt="NFTickets" />
+          <img className="dogsDatabase" src={dogsDatabase} alt="dogsDatabase" />
           <div style={{ position: 'relative', width: '1500px' }}>
-          <h2>Dogs Database</h2>
+            <h2>Dogs Database</h2>
             <br />
             <p className="description">
-              Dogs database is an app that gives all kind of information about different dog races such as temperaments, life span and weight etc. It has a back-end deployed
-              to heroku as well as a postgres database. The front-end is deployed to vercel. <br/>
-              It has a searchbar that work together with different front-end and back-end filters to find any kind of dog. You can also create your own dog and filter by created
-              to find it amongst all the real races. The form to create the dog is fully validated as well as the back-end that gets the post request to add the new entry to
-              the database
+              Dogs database is an app that gives all kind of information about
+              different dog races such as temperaments, life span and weight
+              etc. It has a back-end deployed to heroku as well as a postgres
+              database. The front-end is deployed to vercel. <br />
+              It has a searchbar that work together with different front-end and
+              back-end filters to find any kind of dog. You can also create your
+              own dog and filter by created to find it amongst all the real
+              races. The form to create the dog is fully validated as well as
+              the back-end that gets the post request to add the new entry to
+              the database. <br /> Deployed App:{' '}
+              <a href="https://dogs-database.vercel.app/">
+                Dogs database
+              </a> |{' '}
+              <a href="https://github.com/fedeprat/dogsProject">GitHub repo</a>
+            </p>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="individualProjects">
+          <img
+            className="moviesdatabase"
+            src={moviesdatabase}
+            alt="moviesdatabase"
+          />
+          <div style={{ position: 'relative', width: '1500px' }}>
+            <h2>Movies place</h2>
+            <br />
+            <p className="description">
+              This was one of my first react projects, with it I learnt how to
+              make somehow complex custom hooks to pass information from one
+              component to others. It is a single page app that fetches
+              information about movies from an external API. It uses
+              react-router to navigate. <br />
+              It has a hero image on home section, searchbar and a movie detail
+              section when you click a movie that shows details such as
+              director, casting, year of release, hero image and more.
+              <br /> Deployed App:{' '}
+              <a href="https://movies-place.netlify.app/">
+                Movies database
+              </a> |{' '}
+              <a href="https://github.com/fedeprat/moviesdatabase">
+                GitHub Repo
+              </a>
             </p>
           </div>
         </div>
